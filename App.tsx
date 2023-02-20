6b6de40bd1276e1dd671acc978155d1f64fb3d30
kritbovorn <kritbovorn@gmail.com>
2023-02-20 11:30:09 +0700

    firestore

--- App.tsx
+++ App.tsx
@@ -1,118 +1,54 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import React, { useEffect, useState } from "react";
+import { Text ,View} from 'react-native';
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import firestore from '@react-native-firebase/firestore';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+type MyDataProps = {
+  firstName: string,
+  lastName: string,
+  age: number,
+  hobby: HobbyProps
+}
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
+type HobbyProps = {
 
-function Section({children, title}: SectionProps): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
+}
+
+const App = () => {
+
+  const [myData, setMyData] = useState<MyDataProps | null>(null);
+
+  const getData = async() => {
+    const d = await firestore()
+      .collection("test")
+      .doc("2IM1FdLK5LaMiM46hFyQ")
+      .get();
+
+      const res = d.data() as MyDataProps | null;
+      setMyData(res);
+      console.log("$$$$$ App ::: Line : 15 ::: " +  JSON.stringify(res)); 
+      
+    
+  }
+
+  useEffect(() => {
+
+    getData();
+    
+    return () => {
+      
+    }
+  }, [])
+  
+  return (  
+     <View style={[{flex: 1, alignItems: 'center', justifyContent: 'center'}]} > 
+       <Text style= {[{fontSize: 24}]}>{myData?.firstName } {myData?.lastName} อายุ  {myData?.age}</Text>
+        <Text style= {[]}>{myData?.hobby.map((e) => e + " \n")}</Text>
     </View>
   );
 }
 
-function App(): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-}
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
-
 export default App;
+
+
+
